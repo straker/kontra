60e8768784919497d72cd113cd858f869ad78337
straker <stevenlambert503@gmail.com>
2019-02-18 00:25:59 -0700

    revert commit to built files

--- dist/core.js
+++ dist/core.js
@@ -1,1 +1,1 @@
-!function(){let t={};window.kontra={init(t){let n=this.canvas=document.getElementById(t)||t||document.querySelector("canvas");this.context=n.getContext("2d"),this.context.imageSmoothingEnabled=!1,this.emit("init",this)},on(n,e){t[n]=t[n]||[],t[n].push(e)},emit(n,...e){t[n]&&t[n].forEach(t=>t(...e))},_noop:new Function}}();
+kontra={init(t){var n=this.canvas=document.getElementById(t)||t||document.querySelector("canvas");this.context=n.getContext("2d"),this.context.imageSmoothingEnabled=!1,this._init()},_noop:new Function,_tick:new Function,_init:new Function};

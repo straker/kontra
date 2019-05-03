fab80854f706ea07aaf06a17644654fcf9f69e52
straker <stevenlambert503@gmail.com>
2019-05-03 16:45:14 -0600

    docs: fix code whitespace

--- docs/pages/download.js
+++ docs/pages/download.js
@@ -27,30 +27,30 @@ Download the latest version of Kontra.
 
 As of kontra 6.0.0, custom builds can be achieved by using a module bundler such as [Rollup](https://rollupjs.org/) or [webpack](https://webpack.js.org/). Kontra supports ES modules, allowing you to use [tree-shaking](https://rollupjs.org/guide/en#tree-shaking) to only bundle the code you need.
 
-```js
-// game.js
-import { Sprite, GameLoop } from 'kontra';
-
-let sprite = Sprite({
-  x: 100,
-  y: 100,
-  dx: 2,
-  width: 20,
-  height: 40,
-  color: 'red'
-});
-
-let loop = GameLoop({
-  update() {
-    sprite.update();
-  },
-  render() {
-    sprite.render();
-  }
-});
-
-loop.start();
-```
+* ```js
+* // game.js
+* import { Sprite, GameLoop } from 'kontra';
+*
+* let sprite = Sprite({
+*   x: 100,
+*   y: 100,
+*   dx: 2,
+*   width: 20,
+*   height: 40,
+*   color: 'red'
+* });
+*
+* let loop = GameLoop({
+*   update() {
+*     sprite.update();
+*   },
+*   render() {
+*     sprite.render();
+*   }
+* });
+*
+* loop.start();
+* ```
 
 ```bash
 $ rollup game.js --format iife --file game.bundle.js

784205e0501088a970247eeb36cc370f5f3213c3
Chris Hallberg <crhallberg@gmail.com>
2019-10-02 22:38:26 -0400

    fix(pool): improved pool performance (#119)
    * Switch from hot splicing to sorting in update.
    
    * Trade _i for size.
    
    This simplifies the logic a little but also keeps size up to date with how many items are actually stored and alive in the Pool.
    
    * Switch from hot splicing to sorting in update.
    
    * Trade _i for size.
    
    This simplifies the logic a little but also keeps size up to date with how many items are actually stored and alive in the Pool.
    
    * Adjust tests for new size functionality. One test failing.
    
    - Failing test describes old internals. Not sure what to do about that.
    - 1 to 0
    - Dead at end.
    
    * Optimize sort.
    
    - Make sort function static
    - Slice array so we don't sort the unused tail
    
    * Documentation and naming convention.
    
    * Documentation and naming convention.
    
    * Restore split loop.
    
    * Remove problematic test.
    
    * Inline sort to reduce size and test exposure.

--- test/unit/pool.spec.js
+++ test/unit/pool.spec.js
@@ -110,56 +110,18 @@ describe('pool', () => {
       expect(spy.calledWith(args)).to.be.ok;
     });
 
-    it('should use the first object in the pool and move it to the back of the pool', () => {
-      let pool = Pool({
-        create: sprite,
-        maxSize: 5,
-      });
-
-      // fill the pool
-      pool.get({alive: true});
-      pool.get({alive: true});
-      pool.get({alive: true});
-      pool.get({alive: true});
-      pool.get({alive: true});
-
-      // kill objects in the pool
-      for (let i = 0; i < 5; i++) {
-        pool.objects[i].alive = false;
-      }
-
-      // update pool position
-      pool.update();
-
-      let expected = [
-        pool.objects[1],
-        pool.objects[2],
-        pool.objects[3],
-        pool.objects[4],
-        pool.objects[0]
-      ]
-
-      pool.get();
-
-      expect(pool.objects[0]).to.equal(expected[0]);
-      expect(pool.objects[1]).to.equal(expected[1]);
-      expect(pool.objects[2]).to.equal(expected[2]);
-      expect(pool.objects[3]).to.equal(expected[3]);
-      expect(pool.objects[4]).to.equal(expected[4]);
-    });
-
     it('should increase the size of the pool when there are no more objects', () => {
       let pool = Pool({
         create: sprite
       });
 
-      expect(pool.size).to.equal(1);
+      expect(pool.size).to.equal(0);
 
       pool.get({alive: true});
       pool.get({alive: true});
       pool.get({alive: true});
 
-      expect(pool.size).to.not.equal(1);
+      expect(pool.size).to.not.equal(0);
     });
 
     it('should not increase the size of the pool past the max size', () => {
@@ -188,7 +150,7 @@ describe('pool', () => {
       }
 
       expect(pool.size).to.not.equal(pool.maxSize);
-      expect(pool.size).to.be.equal(256);
+      expect(pool.objects.length).to.be.equal(256);
     });
 
   });
@@ -279,7 +241,7 @@ describe('pool', () => {
       expect(count).to.equal(3);
     });
 
-    it('should move a dead object to the front of the pool', () => {
+    it('should move a dead object to the end of the pool', () => {
       let pool = Pool({
         create: sprite,
         maxSize: 5
@@ -291,11 +253,11 @@ describe('pool', () => {
       pool.objects[2].ttl = 1;
 
       let expected = [
-        pool.objects[2],
         pool.objects[0],
         pool.objects[1],
         pool.objects[3],
-        pool.objects[4]
+        pool.objects[4],
+        pool.objects[2]
       ];
 
       expect(pool.objects[0].isAlive()).to.be.true;
@@ -303,8 +265,9 @@ describe('pool', () => {
       pool.update();
 
       expect(pool.getAliveObjects().length).to.equal(4);
-      expect(pool.objects[0].isAlive()).to.be.false;
-      expect(pool.getAliveObjects().indexOf(pool.objects[0])).to.equal(-1);
+      expect(pool.objects[2].isAlive()).to.be.true;
+      expect(pool.objects[4].isAlive()).to.be.false;
+      expect(pool.getAliveObjects().indexOf(pool.objects[4])).to.equal(-1);
 
       expect(pool.objects[0]).to.equal(expected[0]);
       expect(pool.objects[1]).to.equal(expected[1]);
@@ -374,7 +337,7 @@ describe('pool', () => {
       pool.clear();
 
       expect(pool.objects.length).to.equal(1);
-      expect(pool.size).to.equal(1);
+      expect(pool.size).to.equal(0);
     });
 
   });

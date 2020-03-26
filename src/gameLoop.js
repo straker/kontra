a0baf815a67b62adfab53244e85a0c0a65210d6f
Alex Swan <BoldBigflank@users.noreply.github.com>
2020-03-25 19:29:07 -0500

    Safari 8 Performance bug fix (#154)
    Co-authored-by: Alex Swan <alex@jackboxgames.com>

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -67,6 +67,7 @@ export default function GameLoop({fps = 60, clearCanvas = true, update, render} 
   let step = 1 / fps;
   let clearFn = clearCanvas ? clear : noop
   let last, rAF, now, dt, loop;
+  const performance = window.performance || Date
 
   /**
    * Called every frame of the game loop.

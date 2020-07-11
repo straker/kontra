8fa76f338c3423f699b440c19e09f4ead6cdef56
straker <2433219+straker@users.noreply.github.com>
2020-07-10 21:12:59 -0600

    put docs in right place

--- src/gameObject.js
+++ src/gameObject.js
@@ -478,6 +478,12 @@ class GameObject extends Updatable {
     return this._fop;
   }
 
+  // @ifdef DEBUG
+  set finalOpacity(value) {
+    throw new SyntaxError('finalOpacity is readonly');
+  }
+  // @endif
+
   get opacity() {
     // op = opacity
     return this._op;
@@ -548,3 +554,123 @@ export default function factory() {
 }
 factory.prototype = GameObject.prototype;
 factory.class = GameObject;
+
+
+/**
+ * These are the docs from updatable but because it's private we'll list them here so they are part of GameObject
+ */
+
+
+/**
+ * The game objects position vector. The game objects position is its position in the world, as opposed to the position in the [viewport](api/gameObject#viewX). Typically the position in the world and viewport are the same value. If the game object has been [added to a tileEngine](/api/tileEngine#addObject), the position vector represents where in the tile world the game object is while the viewport represents where to draw the game object in relation to the top-left corner of the canvas.
+ * @memberof GameObject
+ * @property {Vector} position
+ */
+
+/**
+ * The game objects velocity vector.
+ * @memberof GameObject
+ * @property {Vector} velocity
+ */
+
+/**
+ * The game objects acceleration vector.
+ * @memberof GameObject
+ * @property {Vector} acceleration
+ */
+
+/**
+ * How may frames the game object should be alive. Primarily used by [Pool](api/pool) to know when to recycle an object.
+ * @memberof GameObject
+ * @property {Number} ttl
+ */
+
+/**
+ * X coordinate of the position vector.
+ * @memberof GameObject
+ * @property {Number} x
+ */
+
+/**
+ * Y coordinate of the position vector.
+ * @memberof GameObject
+ * @property {Number} y
+ */
+
+/**
+ * Update the game objects position based on its velocity and acceleration. Calls the game objects [advance()](api/gameObject#advance) function.
+ * @memberof GameObject
+ * @function update
+ *
+ * @param {Number} [dt] - Time since last update.
+ */
+
+/**
+ * Move the game object by its acceleration and velocity. If the game object is an [animation game object](api/gameObject#animation-game object), it also advances the animation every frame.
+ *
+ * If you override the game objects [update()](api/gameObject#update) function with your own update function, you can call this function to move the game object normally.
+ *
+ * ```js
+ * import { GameObject } from 'kontra';
+ *
+ * let gameObject = GameObject({
+ *   x: 100,
+ *   y: 200,
+ *   width: 20,
+ *   height: 40,
+ *   dx: 5,
+ *   dy: 2,
+ *   update: function() {
+ *     // move the game object normally
+ *     this.advance();
+ *
+ *     // change the velocity at the edges of the canvas
+ *     if (this.x < 0 ||
+ *         this.x + this.width > this.context.canvas.width) {
+ *       this.dx = -this.dx;
+ *     }
+ *     if (this.y < 0 ||
+ *         this.y + this.height > this.context.canvas.height) {
+ *       this.dy = -this.dy;
+ *     }
+ *   }
+ * });
+ * ```
+ * @memberof GameObject
+ * @function advance
+ *
+ * @param {Number} [dt] - Time since last update.
+ *
+ */
+
+/**
+ * X coordinate of the velocity vector.
+ * @memberof GameObject
+ * @property {Number} dx
+ */
+
+/**
+ * Y coordinate of the velocity vector.
+ * @memberof GameObject
+ * @property {Number} dy
+ */
+
+/**
+ * X coordinate of the acceleration vector.
+ * @memberof GameObject
+ * @property {Number} ddx
+ */
+
+/**
+ * Y coordinate of the acceleration vector.
+ * @memberof GameObject
+ * @property {Number} ddy
+ */
+
+/**
+ * Check if the game object is alive. Used by [Pool](api/pool) to know when to recycle an object.
+ * @memberof GameObject
+ * @function isAlive
+ *
+ * @returns {Boolean} `true` if the game objects [ttl](api/gameObject#ttl) property is above `0`, `false` otherwise.
+ */

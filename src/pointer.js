1dcaf17095beca8f9ee8252bcbb2eb7812dd412b
Steven Lambert <2433219+straker@users.noreply.github.com>
2020-08-17 12:56:07 -0600

    fix(pointer): account for camera (#176)

--- src/pointer.js
+++ src/pointer.js
@@ -101,13 +101,19 @@ export function getPointer(canvas = getCanvas()) {
 }
 
 /**
- * Detection collision between a rectangle and a circlevt.
+ * Detection collision between a rectangle and a circle.
  * @see https://yal.cc/rectangle-circle-intersection-test/
  *
  * @param {Object} object - Object to check collision against.
  */
 function circleRectCollision(object, pointer) {
   let { x, y, width, height } = getWorldRect(object);
+
+  // account for camera
+  do {
+    x -= object.sx || 0;
+    y -= object.sy || 0;
+  } while ((object = object.parent));
 
   let dx = pointer.x - Math.max(x, Math.min(pointer.x, x + width));
   let dy = pointer.y - Math.max(y, Math.min(pointer.y, y + height));

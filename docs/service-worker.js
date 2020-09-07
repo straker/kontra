4d00c4f58fbcd353a2bc31f47133798eb6ead508
straker <2433219+straker@users.noreply.github.com>
2020-09-07 11:41:44 -0600

    docs: show cached pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -7,28 +7,8 @@ if (['localhost', '127.0.0.1'].includes(self.location.hostname
 }
 
 const filesToCache = [
-  // pages (only font caching api docs as others are not as important)
+  // pages
   `${prefix}/`,
-  `${prefix}/api/animation`,
-  `${prefix}/api/assets`,
-  `${prefix}/api/button`,
-  `${prefix}/api/core`,
-  `${prefix}/api/events`,
-  `${prefix}/api/gameLoop`,
-  `${prefix}/api/gameObject`,
-  `${prefix}/api/grid`,
-  `${prefix}/api/helpers`,
-  `${prefix}/api/keyboard`,
-  `${prefix}/api/plugin`,
-  `${prefix}/api/pointer`,
-  `${prefix}/api/pool`,
-  `${prefix}/api/quadtree`,
-  `${prefix}/api/scene`,
-  `${prefix}/api/sprite`,
-  `${prefix}/api/spriteSheet`,
-  `${prefix}/api/text`,
-  `${prefix}/api/tileEngine`,
-  `${prefix}/api/vector`,
 
   // js
   `${prefix}/assets/js/kontra.js`,
@@ -39,7 +19,7 @@ const filesToCache = [
   `${prefix}/assets/styles.css`
 ];
 
-const staticCacheName = 'kontra-docs-v2';
+const staticCacheName = 'kontra-docs-v3';
 
 // cache the application shell
 self.addEventListener('install', event => {

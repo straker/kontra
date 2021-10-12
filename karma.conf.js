cb9f589299abf498d53982986af023e80919dbee
straker <2433219+straker@users.noreply.github.com>
2021-10-11 21:37:58 -0600

    chore: use single karma.conf for debug

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
 // Generated on Tue Apr 07 2015 23:14:35 GMT-0600 (MDT)
+const DEBUG = process.argv.find(argv => argv === '--debug');
 
 module.exports = function (config) {
   config.set({
@@ -20,10 +21,12 @@ module.exports = function (config) {
       { pattern: 'test/unit/*.spec.js', type: 'module' },
       { pattern: 'test/integration/*.spec.js', type: 'module' }
     ],
-    preprocessors: {
-      'src/*.js': ['karma-coverage-istanbul-instrumenter']
-    },
-    browsers: ['ChromeHeadless'],
+    preprocessors: DEBUG
+      ? {}
+      : {
+          'src/*.js': ['karma-coverage-istanbul-instrumenter']
+        },
+    browsers: [DEBUG ? 'Chrome' : 'ChromeHeadless'],
     proxies: {
       '/imgs': '/base/test/imgs',
       '/audio': '/base/test/audio',
@@ -32,10 +35,16 @@ module.exports = function (config) {
     coverageIstanbulInstrumenter: {
       esModules: true
     },
-    reporters: ['mocha', 'coverage-istanbul'],
+    reporters: DEBUG ? ['mocha'] : ['mocha', 'coverage-istanbul'],
     coverageIstanbulReporter: {
       reports: ['html', 'lcovonly', 'text-summary'],
       dir: 'coverage/'
+    },
+    client: {
+      mocha: {
+        timeout: 4000,
+        reporter: 'html'
+      }
     }
   });
 };

e41937d8bb558c0a70a82dadc308bbb59d960d9c
straker <2433219+straker@users.noreply.github.com>
2021-10-14 22:08:22 -0600

    docs: add esbuild-plugin-kontra

--- docs/pages/reducing-file-size.js
+++ docs/pages/reducing-file-size.js
@@ -1,6 +1,6 @@
 /**
 
-By default, the `kontra` export is the entire library. This means that by default Kontra is a bit large. However, Kontra has been written using ES Modules which means you can use a module bundler such as [Rollup](https://rollupjs.org/) or [webpack](https://webpack.js.org/) to take advantage of [tree-shaking](https://rollupjs.org/guide/en#tree-shaking). This will reduce the file size by only bundling the code you need.
+By default, the `kontra` export is the entire library. This means that by default Kontra is a bit large. However, Kontra has been written using ES Modules which means you can use a module bundler such as [Rollup](https://rollupjs.org/) or [esbuild](https://esbuild.github.io/) to take advantage of [tree-shaking](https://rollupjs.org/guide/en#tree-shaking). This will reduce the file size by only bundling the code you need.
 
 * ```js
 * // exclude-tablist
@@ -29,16 +29,16 @@ By default, the `kontra` export is the entire library. This means that by defaul
 * ```
 
 ```bash
-$ rollup game.js --format iife --file game.bundle.js
+# rollup
+rollup game.js --format iife --file game.bundle.js
+
+# esbuild
+esbuild game.js --bundle --format=iife --outfile=game.bundle.js
 ```
 
-## Rollup-plugin-kontra
+## Kontra Bundler Plugins
 
-Kontra supports even more granular control over the file size if tree shaking is not enough. If you're looking to remove unused parts of the Sprite class, such as `rotation` or `anchor`, you can use [rollup-plugin-kontra](https://github.com/straker/rollup-plugin-kontra) to remove entire parts of the Sprite codebase. Just pass the functionality you want enabled and rollup-plugin-kontra will remove the rest.
-
-__Note:__ All options default to `false` when using rollup-plugin-kontra, so only pass the options you wish to enable. Passing no options will result in a Sprite with just `position`, `width,` `height`, and `color`.
-
-__Also Note:__ Some of the more advance classes – such as Button, Scene, and Grid – require some of the gameObject functionality to work properly. If you are using those, you will need to enable the `group` feature or they will not work.
+Kontra supports even more granular control over the file size if tree shaking is not enough. If you're looking to remove unused parts of the Sprite class, such as `rotation` or `anchor`, you can use a kontra bundler plugin to remove entire parts of the Sprite codebase. Just pass the functionality you want enabled and the bundler plugin will remove the rest.
 
 * ```js
 * // exclude-tablist
@@ -66,9 +66,19 @@ __Also Note:__ Some of the more advance classes – such as Button, Scene, and G
 * }
 * ```
 
+### Supported Plugins
+
+* [rollup-plugin-kontra](https://github.com/straker/rollup-plugin-kontra)
+* [esbuild-plugin-kontra](https://github.com/straker/esbuild-plugin-kontra)
+*
+
 ### Available Options
 
 Options are passed by nesting the option with the parent source. For example, to enable the `image` functionality of a sprite, you would pass `sprite: { image: true }`.
+
+All options default to `false`, so only pass the options you wish to enable. Passing no options will result in a Sprite with just `position`, `width,` `height`, and `color`.
+
+__Note:__ Some of the more advance classes – such as Button, Scene, and Grid – require some of the gameObject functionality to work properly. If you are using those, you will need to enable the `group` feature or they will not work.
 
 #### `gameObject` Options:
 

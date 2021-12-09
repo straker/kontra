2382cb76d595fc2ba7c5a1079c26ce1f459be26a
straker <2433219+straker@users.noreply.github.com>
2021-12-09 12:23:17 -0700

    integration test

--- test/integration/scene.spec.js
+++ test/integration/scene.spec.js
@@ -1,6 +1,8 @@
 import Scene from '../../src/scene.js';
+import Sprite from '../../src/sprite.js';
 import TileEngine from '../../src/tileEngine.js';
 import { init, getCanvas } from '../../src/core.js';
+import { depthSort } from '../../src/helpers.js';
 
 describe('Scene integration', () => {
   before(() => {
@@ -30,5 +32,36 @@ describe('Scene integration', () => {
     scene.render();
 
     expect(spy.called).to.be.true;
+  });
+
+  it('should work with helpers.depthSort', () => {
+    let children = [];
+    let spies = [];
+    for (let i = 5; i > 0; i--) {
+      let spy = sinon.spy();
+      spies.push(spy);
+      children.push(
+        Sprite({
+          id: i,
+          x: 5,
+          y: i * 10,
+          width: 15,
+          height: 25,
+          render: spy
+        })
+      );
+    }
+
+    let scene = Scene({
+      id: 'myId',
+      children,
+      sortFunction: depthSort
+    });
+    scene.render();
+
+    expect(spies[4].calledBefore(spies[3])).to.be.true;
+    expect(spies[3].calledBefore(spies[2])).to.be.true;
+    expect(spies[2].calledBefore(spies[1])).to.be.true;
+    expect(spies[1].calledBefore(spies[0])).to.be.true;
   });
 });

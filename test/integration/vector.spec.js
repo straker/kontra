5ac2c857bd85dc169cf2ee03f32c81f3ea942342
Steven Lambert <2433219+straker@users.noreply.github.com>
2022-09-28 23:38:59 -0600

    feat(vector): add set function and allow initialize with object (#324)
    * feat(vector): add set function and allow initialize with object
    
    * integration tests

---
+++ test/integration/vector.spec.js
@@ -0,0 +1,27 @@
+import Vector from '../../src/vector.js';
+import { init, getCanvas } from '../../src/core.js';
+import { rotatePoint, movePoint } from '../../src/helpers.js';
+
+describe('vector integration', () => {
+  before(() => {
+    if (!getCanvas()) {
+      let canvas = document.createElement('canvas');
+      canvas.width = canvas.height = 600;
+      init(canvas);
+    }
+  });
+
+  it('should set from rotatePoint', () => {
+    let vector = Vector(rotatePoint({ x: 10, y: 10 }, Math.PI));
+
+    expect(vector.x).to.be.closeTo(-10, 0.1);
+    expect(vector.y).to.be.closeTo(-10, 0.1);
+  });
+
+  it('should set from movePoint', () => {
+    let vector = Vector(movePoint({ x: 10, y: 10 }, 0, 10));
+
+    expect(vector.x).to.equal(10);
+    expect(vector.y).to.equal(0);
+  });
+});

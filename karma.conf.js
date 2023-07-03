786b6aaca49bd7c7426602a4ca70c81c95ea6263
straker <2433219+straker@users.noreply.github.com>
2023-07-02 20:32:47 -0600

    cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -29,15 +29,9 @@ module.exports = function (config) {
       '/data': '/base/test/data'
     },
     reporters: ['mocha', 'coverage'],
-
     preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
       'src/**/*.js': ['coverage']
     },
-
-    // optionally, configure the reporter
     coverageReporter: {
       check: {
         emitWarning: false,
